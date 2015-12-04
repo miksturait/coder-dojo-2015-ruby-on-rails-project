0be529eaf2f1399ee100f970a305ad57d349d9e0
Michal Czyz <michalczyz@gmail.com>
2015-12-04 16:03:20 +0100

    enabling cors for ember app launched from 192.168.x.x

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost'
+    origins 'localhost', /^http:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$/
 
     resource '*',
       headers: :any,
